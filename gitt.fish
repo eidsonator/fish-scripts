995343022ed8abb9f0f05d9e0002148d689f5a9d
Todd Eidson <todd@eidson.info>
2019-01-30 08:51:32 -0500

    Add flags, resets minor, patch when necessary

--- gitt.fish
+++ gitt.fish
@@ -1,27 +1,64 @@
 function gitt
 
-    if [ (count $argv) = 0 ]
-        echo "You must pass either 'major', 'minor', or 'patch' as the first command line argument"
-        return 1
+    set dry false
+    set build 
+    set type
+    set force false
+    getopts $argv | while read -l key value
+        switch $key
+            case _
+                switch $value
+                    case major minor patch
+                        set type $value
+                        # Handle non-flags here (e.g. `$ foo bam`)
+                    case alpha beta
+                        # Handle `whatever` here
+                        set build $value
+                end
+            case -w
+                echo 'wahtever'
+            case d dry
+                set dry true
+            case f force
+                echo force
+                set force true
+            case v version
+                _gitt_print_version
+                return 0
+            case \*
+                echo unknown flag $key
+
+                # Handle unknown flags here
+        end
     end
 
     set v (git describe --abbrev=0 --tags)
+    # strip any thing from the hyphen after to remove -alpha or -beta
+    set v (string split "-" -- $v)[1]
     set vmajor (string split "." -- $v)[1]
     set vminor (string split "." -- $v)[2]
     set vpatch (string split "." -- $v)[3]
 
-    if [ $argv[1] = 'patch' ]
+    if [ $type = 'patch' ]
         set vpatch (math $vpatch + 1)
-    else if [ $argv[1] = 'minor' ]
+    else if [ type = 'minor' ]
         set vminor (math $vminor + 1)
-    else if [ $argv[1] = 'major' ]
+        set vpatch 0
+    else if [ $type = 'major' ]
         set vmajor (math $vmajor + 1)
+        set vpatch 0
+        set vminor 0        
+    else
+        echo "Help"
+        echo "Accepts 'major', 'minor', or 'patch' as the first command line argument"
+        
+        return 0
     end
 
-    if [ (count $argv) = 2 ]
-        if [ $argv[2] = 'alpha' ]
+    if [ $build ]
+        if [ $build = 'alpha' ]
             set build alpha
-        else if [ $argv[2] = 'beta' ]
+        else if [ $build = 'beta' ]
             set build beta
         else
             echo "The 'build' can only be blank, alpha, or beta"
@@ -32,7 +69,22 @@ function gitt
         set tag $vmajor.$vminor.$vpatch
     end
 
-    git tag $tag
+    if $dry
+        echo $tag
+    else 
+        if git_is_dirty            
+            if $force
+                echo 'Forcing new version'
+            else
+                echo 'git is dirty - must use --force'
+                return 1
+            end
+        end
+        git tag $tag
+        git push origin master --tags
+    end
+end
 
-    git push origin master --tags
+function _gitt_print_version
+    echo gitt verison increment 1.0.0
 end
